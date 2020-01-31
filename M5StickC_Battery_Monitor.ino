b053c54c9132745333caf23fd29d6bb3ce44f029
MDE <59321583+mdevel1@users.noreply.github.com>
2020-01-31 01:08:57 +0100

    log: Improved readability

--- M5StickC_Battery_Monitor.ino
+++ M5StickC_Battery_Monitor.ino
@@ -40,26 +40,27 @@ void setup() {
 
 
 void loop() {
-    log_all("BatU:%8.3f\r\n", M5.Axp.GetBatVoltage());
-    log_all("BatI:%8.3f\r\n", M5.Axp.GetBatCurrent());
-    log_all("VinU:%8.3f\r\n", M5.Axp.GetVinVoltage());
-    log_all("VinI:%8.3f\r\n", M5.Axp.GetVinCurrent());
-    log_all("VBusU:%7.3f\r\n", M5.Axp.GetVBusVoltage());
-    log_all("VBusI:%7.3f\r\n", M5.Axp.GetVBusCurrent());
+    log_all("BatU%9.3f\r\n", M5.Axp.GetBatVoltage());
+    log_all("BatI%9.3f\r\n", M5.Axp.GetBatCurrent());
+    log_all("VinU%9.3f\r\n", M5.Axp.GetVinVoltage());
+    log_all("VinI%9.3f\r\n", M5.Axp.GetVinCurrent());
+    log_all("VBusU%8.3f\r\n", M5.Axp.GetVBusVoltage());
+    log_all("VBusI%8.3f\r\n", M5.Axp.GetVBusCurrent());
     log_serial("\r\n");
-    log_all("TInAXP: %.1fC\r\n", M5.Axp.GetTempInAXP192());
+    log_all("TInAXP%6.2fC\r\n", M5.Axp.GetTempInAXP192());
     log_serial("\r\n");
-    log_all("BatP: %7.3f\r\n", M5.Axp.GetBatPower());
-    log_all("BatChargeI: %14.3f\r\n", M5.Axp.GetBatChargeCurrent());
-    log_all("APSU: %7.3f\r\n", M5.Axp.GetAPSVoltage());
-    log_all("BatCoulombIn:%13.3f\r\n", M5.Axp.GetBatCoulombInput());
-    log_all("BatCoulombOut:%12.3f\r\n", M5.Axp.GetBatCoulombOut());
-    log_serial("CoulombchargeData: %ld\r\n", M5.Axp.GetCoulombchargeData());
-    log_serial("CoulombdischargeData: %ld\r\n", M5.Axp.GetCoulombdischargeData());
-    log_all("CoulombData: %13.3f\r\n", M5.Axp.GetCoulombData());
+    log_all("APSU%9.3f\r\n", M5.Axp.GetAPSVoltage());
+    log_all("BatP%9.3f\r\n", M5.Axp.GetBatPower());
+    log_all("BatChargeI%16.3f\r\n", M5.Axp.GetBatChargeCurrent());
     log_serial("\r\n");
-    log_all("BatState:%4d\r\n", M5.Axp.GetBatState());
-    log_all("WarningLevel:%13d\r\r\n", M5.Axp.GetWarningLevel());
+    log_all("BatCoulombIn%14.3f\r\n", M5.Axp.GetBatCoulombInput());
+    log_all("BatCoulombOut%13.3f\r\n", M5.Axp.GetBatCoulombOut());
+    log_serial("CoulombchargeData    %ld\r\n", M5.Axp.GetCoulombchargeData());
+    log_serial("CoulombdischargeData %ld\r\n", M5.Axp.GetCoulombdischargeData());
+    log_all("CoulombData%15.3f\r\n", M5.Axp.GetCoulombData());
+    log_serial("\r\n");
+    log_all("BatState%5d\r\n", M5.Axp.GetBatState());
+    log_all("WarningLevel%14d\r\r\n", M5.Axp.GetWarningLevel());
 
     log_serial("-----\r\n");
     M5.Lcd.setCursor(0, 0);
